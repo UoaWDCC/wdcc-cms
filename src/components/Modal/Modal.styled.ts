36248e3510443be013eb8da6dc05eb72aafcf375
Jacob Tan <jacob.tan229@gmail.com>
2022-05-16 00:23:15 +1200

    feat(draft): modal has draft level functionality

---
+++ src/components/Modal/Modal.styled.ts
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 20;
+`;
+
+export const ModalWrapper = styled.div`
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  z-index: 30;
+  border-radius: 10px;
+  overflow-y: auto;
+`;
+
+export const ModalImg = styled.img`
+  width: 80%;
+  height: 40vh;
+  background-size: cover;
+  border-radius: 3rem;
+  padding: 2rem;
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.3;
+  color: #141414;
+
+  h5 {
+    text-transform: uppercase;
+    color: rgba(0, 0, 0, 0.5);
+  }
+  h1 {
+    text-transform: uppercase;
+    margin-top: 0.2em;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+export const CloseModalButton = styled.div`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
+export const Information = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-transform: capitalize;
+  font-weight: bold;
+`;
+
+export const Container = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  p {
+    color: #6a6666;
+  }
+`;
--- src/pages/EventsPage/EventsPage.tsx
+++ src/pages/EventsPage/EventsPage.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
+import Modal from "../../components/Modal/Modal";
 import NavBar from "../../components/NavBar/NavBar";
 import Screen from "../../styles/Screen";
 
 function EventsPage() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const modalButton = () => {
+    setOpenModal((prev) => !prev);
+  };
   return (
     <>
+      {openModal && <Modal modalButton={modalButton} />}
       <NavBar hasBg={true} />
+      <button onClick={modalButton}>click me to open modal!</button>
       <Screen>Events!!</Screen>
+
       <Footer />
     </>
   );
