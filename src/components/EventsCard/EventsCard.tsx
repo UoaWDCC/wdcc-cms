41ef4ee4b210f85415727f518e4f22db3dcd091a
Jolin <jche521@aucklanduni.ac.nz>
2022-05-16 01:24:08 +1200

    formatting using prettier

--- src/components/EventsCard/EventsCard.tsx
+++ src/components/EventsCard/EventsCard.tsx
@@ -3,33 +3,41 @@ import { Container } from "./EventsCard.styled";
 interface EventsCardProps {
   photo: any;
   month: string;
-	date: string;
-	series: string;
+  date: string;
+  series: string;
   title: string;
-	location: string;
-	time: string;
+  location: string;
+  time: string;
 }
 
-function EventsCard({photo, month, date, series, title, location, time}: EventsCardProps) {
-	return (
-		<Container>
-			<div>
-				<img src={photo}/>
-			</div>
-			<div className="text">
-				<div className="black">
-					<p>{month}</p>
-					<h2>{date}</h2>
-				</div>
-				<div className="blue">
-					<p>{series}</p>
-					<h1>{title}</h1>
-					<p>{location}</p>
-					<p>{time}</p>
-				</div>
-			</div>
-		</Container>
-	)
+function EventsCard({
+  photo,
+  month,
+  date,
+  series,
+  title,
+  location,
+  time,
+}: EventsCardProps) {
+  return (
+    <Container>
+      <div>
+        <img src={photo} />
+      </div>
+      <div className="text">
+        <div className="black">
+          <p>{month}</p>
+          <h2>{date}</h2>
+        </div>
+        <div className="blue">
+          <p>{series}</p>
+          <h1>{title}</h1>
+          <p>{location}</p>
+          <p>{time}</p>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default EventsCard;
