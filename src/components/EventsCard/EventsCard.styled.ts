d489a70d1d4ad7e3312d11ca20c19766155e1964
Vella BW <marvellaaabw@gmail.com>
2022-05-15 13:25:03 +1200

    Created events card component

---
+++ src/components/EventsCard/EventsCard.styled.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  margin: 0 20px;
+  // padding: 30px;
+	border-radius: 30px;
+  color: white;
+
+  // width: 400px;
+  // max-width: 100%;
+  // min-width: 300px;
+
+  // &:nth-child(2n) {
+  //   background-color: #f5f5f5;
+  // }
+
+  div {
+    // height: 150px;
+    // width: 200px;
+    // margin: 0 auto 50px auto;
+  }
+
+  img {
+    max-width: 100%;
+    width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  p {
+    text-align: left;
+    line-height: 2rem;
+  }
+
+	.text {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.black {
+		background-color: black;
+    padding: 2rem 2rem 2rem 2rem;
+	}
+
+	.blue {
+		background-color: blue;
+    padding: 2rem 2rem 2rem 2rem;
+	}
+`;
--- src/pages/EventsPage/EventsPage.tsx
+++ src/pages/EventsPage/EventsPage.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
 import Screen from "../../styles/Screen";
+import EventsCard from "../../components/EventsCard/EventsCard";
 
 function EventsPage() {
   return (
     <>
       <NavBar hasBg={true} />
       <Screen>Events!!</Screen>
+      <EventsCard photo="" month="Apr" date="8" series="Web Dev Essentials" title="HTML/CSS Workshop" location="201-439 Eng Building" time="10am-4pm"></EventsCard>
       <Footer />
     </>
   );
