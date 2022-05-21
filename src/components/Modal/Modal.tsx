d987732887c60152a523998fd23f2d63239b4f93
Jacob Tan <jacob.tan229@gmail.com>
2022-05-21 22:15:56 +1200

    chore: removed code in events page

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -16,7 +16,30 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Modal = ({ open, handleClose }: any) => {
+interface ModalProps {
+  open: boolean;
+  handleClose: () => void;
+  information: string;
+  photo: string;
+  schedule: {
+    date: string;
+    time: string;
+  };
+  series: string;
+  title: string;
+  location: string;
+}
+
+const Modal = ({
+  open,
+  handleClose,
+  information,
+  photo,
+  schedule,
+  series,
+  title,
+  location,
+}: ModalProps) => {
   // Logic which handles modal close upon clicking the background
   const modalRef: any = useRef();
   const closeModal = (e: any) => {
@@ -44,41 +67,26 @@ const Modal = ({ open, handleClose }: any) => {
 
             {/* Title */}
             <ModalContent>
-              <h5>web dev essentials</h5>
-              <h1>html/css workshop</h1>
+              <h5>{series}</h5>
+              <h1>{title}</h1>
             </ModalContent>
 
             {/* Image */}
-            <ModalImg
-              src={
-                "https://media.itpro.co.uk/image/upload/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1627554145/itpro/google_sign_mountain_view_shutterstock.jpg"
-              }
-            />
+            <ModalImg src={photo} />
 
             {/* Contains location, time and extended details */}
             <Container>
               <Information>
                 <div>
                   <FontAwesomeIcon icon={faLocationDot} />
-                  401-439 engineering building
+                  {location}
                 </div>
                 <div>
                   <FontAwesomeIcon icon={faClockFour} />
-                  sat 02 apr, 10:00am - 4:00pm
+                  {schedule.date + "," + schedule.time}
                 </div>
               </Information>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
-                enim quibusdam magni voluptatibus dicta quas error minima porro
-                alias eius, dolorum dolorem commodi officia soluta vitae, odio
-                rem suscipit iusto a beatae fugit deserunt molestiae laborum
-                voluptatum. Enim laudantium sit vero eaque, reiciendis illum
-                officiis libero ad, possimus dolores iusto? Vitae dignissimos
-                repellat eligendi laudantium ipsum sint cum! Officia possimus
-                reiciendis atque voluptates ratione hic, nemo excepturi autem
-                porro, corrupti sint officiis! Accusamus, fuga illum,
-                repellendus iusto officiis necessitatibus dicta maiores eos quas
-              </p>
+              <p>{information}</p>
             </Container>
           </ModalWrapper>
         </ModalBackground>
--- src/pages/EventsPage/EventsPage.tsx
+++ src/pages/EventsPage/EventsPage.tsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
-import Modal from "../../components/Modal/Modal";
 import NavBar from "../../components/NavBar/NavBar";
 import Screen from "../../styles/Screen";
 
 function EventsPage() {
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => setOpen(true);
-
-  const handleClose = () => setOpen(false);
   return (
     <>
-      <Modal handleClose={handleClose} open={open} />
-
       <NavBar hasBg={true} />
-      <button onClick={handleOpen}>click me to open modal!</button>
       <Screen>Events!!</Screen>
-
       <Footer />
     </>
   );
