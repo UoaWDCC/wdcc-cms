a07a0a8a88b4a382d93f2001b0d20b45b3abb3fa
Jacob Tan <jacob.tan229@gmail.com>
2022-05-22 09:18:04 +1200

    feat: styling and containerising the cards on the events page

--- src/pages/EventsPage/EventsPage.styled.ts
+++ src/pages/EventsPage/EventsPage.styled.ts
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-around;
   align-items: center;
+  padding: 2rem;
+  background-color: #f4f4f4;
+  margin: 2rem;
+  height: 100vh;
 `;
